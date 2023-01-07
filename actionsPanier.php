8d3787fa0f8f4308b1b9962bc852a3472b8000c6
mchabanat <matischabanat@gmail.com>
2023-01-07 14:47:53 +0100

    Reglage bug panier + ajout fonctionnalités panier

--- actionsPanier.php
+++ actionsPanier.php
@@ -4,14 +4,13 @@ session_start();
 $id = $_GET['id'];
 $mode = $_GET['mode'];
 
-//Retirer un CD d'id id
-
+// On regarde si le tableau est déclaré
 if (isset($_SESSION['eltsPanier'])) {
     // On regarde si le tableau est vide
     if (sizeof($_SESSION['eltsPanier']) != 0) {
         // S'il n'est pas vide, on regarde quelle action il faut faire sur l'article en question
         switch($mode) {
-            case $mode=1: // On enlève un exemplaire
+            case 1: // On enlève un exemplaire
                 if ($_SESSION['eltsPanier'][$id] == 1) {
                     // S'il y est en un seul exemplaire alors on le supprime du panier
                     unset($_SESSION['eltsPanier'][$id]);
@@ -20,11 +19,15 @@ if (isset($_SESSION['eltsPanier'])) {
                     $_SESSION['eltsPanier'][$id]--;
                 }
                 break;
-            case $mode=2: // On ajoute un exemplaire
+            case 2: // On ajoute un exemplaire
                 $_SESSION['eltsPanier'][$id]++;
                 break;
-            case $mode=3: // On supprime du panier
+            case 3: // On supprime du panier
                 unset($_SESSION['eltsPanier'][$id]);
+                break;
+            case 4: // On vide le panier
+                // Réinitialisation du panier à un tableau vide
+                $_SESSION["eltsPanier"] = array();
                 break;
         }
         header("Location: panier.php");
